d7a8603ca351699d4e95b2a4676493c2b0a6610d
Gesang Paudra J <g.paudra@gmail.com>
2023-10-03 17:16:36 +0700

    fix admin register

--- src/pages/register-admin/InputAdmin.js
+++ src/pages/register-admin/InputAdmin.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import InputAdornment from '@mui/material/InputAdornment';
+import React, { useState } from 'react'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
 import Alert from '@mui/material/Alert'
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 
 const FormAdmin = () => {
-  const [nama, setnama] = useState('');
-  const [email, setemail] = useState('');
-  const [password, setpassword] = useState('');
-  const [roles, setRoles] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState(''); // State variable for error message
+  const [nama, setnama] = useState('')
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
-  const handlepasswordChange = (e) => setpassword(e.target.value);
-  const handlenamaChange = (e) => setnama(e.target.value);
-  const handleemailChange = (e) => setemail(e.target.value);
+  const handlepasswordChange = (e) => setpassword(e.target.value)
+  const handlenamaChange = (e) => setnama(e.target.value)
+  const handleemailChange = (e) => setemail(e.target.value)
 
   const handleChange = (event) => {
     setRoles(event.target.value);
@@ -34,9 +27,9 @@ const FormAdmin = () => {
     e.preventDefault();
 
     // Validate the form fields
-    if (!nama || !email || !password || !roles) {
+    if (!nama || !email || !password) {
       // Display an error message if any field is empty
-      setErrorMessage('All fields are required');
+      setErrorMessage('Semua kolom harus diisi!');
 
       return;
     }
@@ -44,12 +37,11 @@ const FormAdmin = () => {
     const AkunData = {
       nama,
       email,
-      password,
-      roles, // Include the selected role
+      password
     };
 
     try {
-      const response = await fetch('http://localhost:3001/api/tambah-akun', {
+      const response = await fetch('http://localhost:3001/api/tambah-admin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -58,16 +50,15 @@ const FormAdmin = () => {
       });
 
       if (response.ok) {
-        setSuccessMessage(`Akun ${nama} sebagai ${roles} berhasil ditambahkan.`);
+        setSuccessMessage(`Admin ${nama} berhasil ditambahkan.`);
         setnama('');
         setemail('');
         setpassword('');
-        setRoles(''); // Reset the role selection
         setTimeout(() => {
           setSuccessMessage('');
         }, 5000);
       } else {
-        console.error('Error adding akun.');
+        console.error('Error menambahkan Admin.');
       }
     } catch (error) {
       console.error('Error:', error);
@@ -76,7 +67,7 @@ const FormAdmin = () => {
 
   return (
     <Card>
-      <CardHeader title='Form Tambah Akun' titleTypographyProps={{ variant: 'h6' }} />
+      <CardHeader title='Form Tambah Admin' titleTypographyProps={{ variant: 'h6' }} />
       <CardContent>
         {errorMessage && (
           <Alert severity="error">{errorMessage}</Alert>
@@ -90,10 +81,10 @@ const FormAdmin = () => {
               <TextField
                 fullWidth
                 type='text'
-                label='Nama Akun'
+                label='Nama Admin'
                 name='nama'
-                placeholder='Nama Akun'
-                helperText='Masukkan Nama Akun'
+                placeholder='Nama Admin'
+                helperText='Masukkan Nama Admin'
                 value={nama}
                 onChange={handlenamaChange}
               />
@@ -105,7 +96,7 @@ const FormAdmin = () => {
                 label='Email'
                 name='email'
                 placeholder='Email'
-                helperText='Masukkan Email Akun'
+                helperText='Masukkan Email Admin'
                 value={email}
                 onChange={handleemailChange}
               />
@@ -116,31 +107,11 @@ const FormAdmin = () => {
                 type='password'
                 label='Password'
                 name='password'
-                placeholder='Password Akun'
-                helperText='Masukkan Password Akun'
+                placeholder='Password Admin'
+                helperText='Masukkan Password Admin'
                 value={password}
                 onChange={handlepasswordChange}
               />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id="roles">Kategori</InputLabel>
-                <Select
-                  labelId="roles"
-                  id="roles"
-                  value={roles}
-                  label="Kategori"
-                  onChange={handleChange}
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value="admin">Admin</MenuItem>
-                  <MenuItem value="user">User</MenuItem>
-                  <MenuItem value="super-admin">Super Admin</MenuItem>
-                </Select>
-                <FormHelperText>Pilih jenis akun</FormHelperText>
-              </FormControl>
             </Grid>
             <Grid item xs={12}>
               <Box
@@ -153,7 +124,7 @@ const FormAdmin = () => {
                 }}
               >
                 <Button type='submit' variant='contained' size='large'>
-                  Tambah Akun
+                  Tambah Admin
                 </Button>
               </Box>
             </Grid>
