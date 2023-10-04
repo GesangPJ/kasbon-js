2dccf835f8c0db74f03c9db3fa3f05bd3542dd47
Gesang Paudra J <g.paudra@gmail.com>
2023-10-04 11:23:55 +0700

    show session name on appbar

--- src/layouts/components/vertical/AppBarContent.js
+++ src/layouts/components/vertical/AppBarContent.js
@@ -7,6 +7,9 @@ import ModeToggler from 'src/@core/layouts/components/shared-components/ModeTogg
 import UserDropdown from 'src/@core/layouts/components/shared-components/UserDropdown';
 import NotificationDropdown from 'src/@core/layouts/components/shared-components/NotificationDropdown';
 import SearchBar from './SearchBar'; // Import the SearchBar component
+import Typography from '@mui/material/Typography'
+import { useEffect, useState } from 'react';
+
 
 const AppBarContent = (props) => {
   // ** Props
@@ -15,9 +18,35 @@ const AppBarContent = (props) => {
   // ** Hook
   const hiddenSm = useMediaQuery((theme) => theme.breakpoints.down('sm'));
 
+  const [user, setUser] = useState({ nama: '' });
+  const [admin, setAdmin] = useState({ nama: '' });
+
+  useEffect(() => {
+    // Fetch user information from the session when the component mounts
+    async function fetchUserInfo() {
+      try {
+        const response = await fetch('http://localhost:3001/api/session', {
+          method: 'GET',
+          credentials: 'include',
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data.user);
+        }
+      } catch (error) {
+        console.error('Error fetching user info:', error);
+      }
+    }
+
+    fetchUserInfo();
+  }, []);
+
+
+
   return (
     <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space between' }}>
-      <Box className="actions-left" sx={{ mr: 2, display: 'flex', alignItems: 'center' }}>
+      <Box className="actions-left" sx={{ mr: 2, display: 'flex', justifyContent: 'flex-end' }}>
         {hidden ? (
           <IconButton
             color="inherit"
@@ -29,13 +58,16 @@ const AppBarContent = (props) => {
         ) : null}
         {/*<SearchBar />*/} {/* New Custom Search Bar */}
       </Box>
-      <Box className="actions-right" sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <ModeToggler settings={settings} saveSettings={saveSettings} />
-        <NotificationDropdown />
-        <UserDropdown />
+      <Box className="actions-right" sx={{ display: 'flex', justifyContent: 'flex-end', flex: 1 }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <ModeToggler settings={settings} saveSettings={saveSettings} />
+          <NotificationDropdown />
+          <UserDropdown />
+          <Typography variant="body1" sx={{ fontWeight: 600 }}>{user.nama || (admin.nama ? admin.nama : 'User')}</Typography>
+        </div>
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default AppBarContent;
+export default AppBarContent
