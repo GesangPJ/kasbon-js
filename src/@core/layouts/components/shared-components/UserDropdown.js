2bf178a1e33461f138db7cf0ad26fd1c2e68557b
Gesang Paudra J <g.paudra@gmail.com>
2023-10-04 09:35:42 +0700

    update dropdown

--- src/@core/layouts/components/shared-components/UserDropdown.js
+++ src/@core/layouts/components/shared-components/UserDropdown.js
@@ -1,5 +1,5 @@
 // ** React Imports
-import { useState, Fragment } from 'react'
+import { useState, useEffect, Fragment } from 'react'
 
 // ** Next Import
 import { useRouter } from 'next/router'
@@ -32,32 +32,30 @@ const BadgeContentSpan = styled('span')(({ theme }) => ({
   boxShadow: `0 0 0 2px ${theme.palette.background.paper}`
 }))
 
-const UserDropdown = (props) => {
-  // ** States
-  const [anchorEl, setAnchorEl] = useState(null)
+const UserDropdown = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [user, setUser] = useState(null); // State to store user information
+  const router = useRouter();
 
-  // ** Hooks
-  const router = useRouter()
+  const handleDropdownOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-  const handleDropdownOpen = event => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleDropdownClose = url => {
+  const handleDropdownClose = (url) => {
     if (url) {
-      router.push(url)
+      router.push(url);
     }
-    setAnchorEl(null)
-  }
+    setAnchorEl(null);
+  };
 
   const handleLogout = async () => {
     try {
       await fetch('http://localhost:3001/api/logout', {
         method: 'GET',
         credentials: 'include',
-      })
+      });
 
-      router.push('/'); // kirim user/admin ke login
+      router.push('/'); // Redirect user/admin to the login page
     } catch (error) {
       console.error('Error during logout:', error);
 
@@ -65,38 +63,43 @@ const UserDropdown = (props) => {
     }
 
     setAnchorEl(null); // Close the dropdown
-  }
+  };
 
-  //const UserName = req.session.user ? req.session.user.nama_user : 'User'
+  useEffect(() => {
+    // Fetch user information from the session when the component mounts
+    async function fetchUserInfo() {
+      try {
+        const response = await fetch('http://localhost:3001/api/login', {
+          method: 'POST',
+          credentials: 'include',
+        });
 
-  const styles = {
-    py: 2,
-    px: 4,
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    color: 'text.primary',
-    textDecoration: 'none',
-    '& svg': {
-      fontSize: '1.375rem',
-      color: 'text.secondary'
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data.user);
+        }
+      } catch (error) {
+        console.error('Error fetching user info:', error);
+      }
     }
-  }
+
+    fetchUserInfo();
+  }, []);
 
   return (
     <Fragment>
       <Badge
-        overlap='circular'
+        overlap="circular"
         onClick={handleDropdownOpen}
         sx={{ ml: 2, cursor: 'pointer' }}
         badgeContent={<BadgeContentSpan />}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
       >
         <Avatar
-          alt='John Doe'
+          alt="User"
           onClick={handleDropdownOpen}
           sx={{ width: 40, height: 40 }}
-          src='/images/avatars/admin-icon.png'
+          src="/images/avatars/user-icon.png"
         />
       </Badge>
       <Menu
@@ -110,15 +113,19 @@ const UserDropdown = (props) => {
         <Box sx={{ pt: 2, pb: 3, px: 4 }}>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Badge
-              overlap='circular'
+              overlap="circular"
               badgeContent={<BadgeContentSpan />}
               anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
             >
-              <Avatar alt='Admin Icon' src='/images/avatars/admin-icon.png' sx={{ width: '2.5rem', height: '2.5rem' }} />
+              <Avatar
+                alt="User Icon"
+                src="/images/avatars/user-icon.png"
+                sx={{ width: '2.5rem', height: '2.5rem' }}
+              />
             </Badge>
             <Box sx={{ display: 'flex', marginLeft: 3, alignItems: 'flex-start', flexDirection: 'column' }}>
-              <Typography sx={{ fontWeight: 600 }}>{props.user.nama_user}</Typography>
-              <Typography variant='body2' sx={{ fontSize: '0.8rem', color: 'text.disabled' }}>
+              <Typography sx={{ fontWeight: 600 }}>{user ? user.nama_user : 'User'}</Typography>
+              <Typography variant="body2" sx={{ fontSize: '0.8rem', color: 'text.disabled' }}>
                 Akun
               </Typography>
             </Box>
@@ -139,7 +146,7 @@ const UserDropdown = (props) => {
         </MenuItem>
       </Menu>
     </Fragment>
-  )
-}
+  );
+};
 
-export default UserDropdown
+export default UserDropdown;
