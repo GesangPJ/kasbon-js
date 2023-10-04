8a416a7d1e94da6fcf8c0f264b373ce15e7b54cf
Gesang Paudra J <g.paudra@gmail.com>
2023-10-04 14:12:39 +0700

    dynamically change sidebar depends on session

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -15,6 +15,7 @@ import VerticalAppBarContent from './components/vertical/AppBarContent'
 
 // ** Hook Import
 import { useSettings } from 'src/@core/hooks/useSettings'
+import React, { useEffect, useState } from 'react'
 
 const UserLayout = ({ children }) => {
   // ** Hooks
@@ -29,29 +30,47 @@ const UserLayout = ({ children }) => {
    *  ! Do not change this value unless you know what you are doing. It can break the template.
    */
   const hidden = useMediaQuery(theme => theme.breakpoints.down('lg'))
+  const [userRole, setUserRole] = useState(null)
 
-  const UpgradeToProImg = () => {
-    /**return (
+  // Fetch user role from the session
+  useEffect(() => {
+    async function fetchUserRole() {
+      try {
+        const response = await fetch('http://localhost:3001/api/session', {
+          method: 'GET',
+          credentials: 'include',
+        });
 
-      <Box sx={{ mx: 'auto' }}>
-        <a
-          target='_blank'
-          rel='noreferrer'
-          href='https://themeselection.com/products/materio-mui-react-nextjs-admin-template/'
-        >
-          <img width={230} alt='upgrade to premium' src={`/images/misc/upgrade-banner-${settings.mode}.png`} />
-        </a>
-      </Box>
-    )*/
-  }
+        if (response.ok) {
+          const data = await response.json();
+          setUserRole(data.user ? 'user' : data.admin ? 'admin' : null);
+        }
+      } catch (error) {
+        console.error('Error fetching user role:', error);
+      }
+    }
+
+    fetchUserRole();
+  }, [])
+
+  const getNavigationItems = () => {
+    if (userRole === 'admin') {
+      return AdminNavigation();
+    } else if (userRole === 'user') {
+      return VerticalNavItems();
+    } else {
+      // Return empty navigation items if the user is not logged in.
+      return [];
+    }
+  };
 
   return (
     <VerticalLayout
       hidden={hidden}
       settings={settings}
       saveSettings={saveSettings}
-      verticalNavItems={VerticalNavItems()} // Navigation Items
-      //afterVerticalNavMenuContent={UpgradeToProImg}
+      verticalNavItems={getNavigationItems()} // Mengubah sidebar sesuai session
+
       verticalAppBarContent={(
         props // AppBar Content
       ) => (
