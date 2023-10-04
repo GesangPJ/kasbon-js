2ddf4183da6e27a348775776a4d0fa871dda7fa7
Gesang Paudra J <g.paudra@gmail.com>
2023-10-04 09:41:29 +0700

    fix styles error

--- src/@core/layouts/components/shared-components/UserDropdown.js
+++ src/@core/layouts/components/shared-components/UserDropdown.js
@@ -65,6 +65,20 @@ const UserDropdown = () => {
     setAnchorEl(null); // Close the dropdown
   };
 
+  const styles = {
+    py: 2,
+    px: 4,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    color: 'text.primary',
+    textDecoration: 'none',
+    '& svg': {
+      fontSize: '1.375rem',
+      color: 'text.secondary'
+    }
+  }
+
   useEffect(() => {
     // Fetch user information from the session when the component mounts
     async function fetchUserInfo() {
