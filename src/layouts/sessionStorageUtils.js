64c0ab36a641ba3ff4f6f8627775e415361d3876
Gesang Paudra J <g.paudra@gmail.com>
2023-10-05 07:07:05 +0700

    update sessionstorage util to not run when outside session

--- src/layouts/sessionStorageUtils.js
+++ src/layouts/sessionStorageUtils.js
@@ -1,7 +1,13 @@
 // sessionStorageUtils.js
 
 export function getIsAdminFromSessionStorage() {
-  const sessionData = JSON.parse(sessionStorage.getItem('sessionData'));
+  // Check if we're running in the browser environment
+  if (typeof window !== 'undefined') {
+    const sessionData = JSON.parse(sessionStorage.getItem('sessionData'));
 
-  return sessionData && sessionData.isAdmin;
+    return sessionData && sessionData.isAdmin;
+  }
+
+  // Handle non-browser environments gracefully
+  return false; // Set a default value or handle it as needed
 }
