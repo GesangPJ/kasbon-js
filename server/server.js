ac3279d255ccf899a55e847efb4ef973eb6905ce
Gesang Paudra J <g.paudra@gmail.com>
2023-10-06 10:28:22 +0700

    add api/ambil-dashboard-karyawan

--- server/server.js
+++ server/server.js
@@ -348,6 +348,37 @@ app.post('/api/input-kasbon', async (req, res) => {
   }
 })
 
+// API Ambil Dashboard_Karyawan berdasarkan id_karyawan
+app.post('/api/ambil-dashboard-karyawan', async (req, res) => {
+  const { id_akun } = req.body
+
+  try {
+    const client = await pool.connect()
+
+    // Query ambil data
+    const selectQuery = 'SELECT * FROM dashboard_karyawan WHERE id_karyawan = $1'
+    const selectResult = await client.query(selectQuery, [id_akun])
+
+    client.release()
+
+    if (selectResult.rowCount > 0) {
+      // Data found, send the query result
+      res.status(200).json(selectResult.rows);
+    } else {
+      // Data not found, send a message
+      res.status(404).json({ message: 'Data not found' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+
+
+
+
+
 // Set Port buat server
 const port = process.env.PORT || 3001;
 app.listen(port, async () => {
