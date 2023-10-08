695a5ca0ef45044894a7bf95cdb9241bf815c55a
Gesang Paudra J <g.paudra@gmail.com>
2023-10-08 13:07:58 +0700

    update admin sidebar

--- src/navigation/vertical/admin-nav.js
+++ src/navigation/vertical/admin-nav.js
@@ -1,14 +1,9 @@
 // ** Icon imports
-import Login from 'mdi-material-ui/Login'
-import Table from 'mdi-material-ui/Table'
 import { MonitorHeart } from '@mui/icons-material'
 import { AddCircle } from '@mui/icons-material'
-import { ModeEdit } from '@mui/icons-material'
 import SpaceDashboardOutlinedIcon from '@mui/icons-material/SpaceDashboardOutlined'
-import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined'
 import RequestQuoteOutlinedIcon from '@mui/icons-material/RequestQuoteOutlined'
 import PaidIcon from '@mui/icons-material/Paid'
-import TextSnippetOutlinedIcon from '@mui/icons-material/TextSnippetOutlined'
 import SaveAltOutlinedIcon from '@mui/icons-material/SaveAltOutlined'
 
 const AdminNavigation = () => {
@@ -49,12 +44,6 @@ const AdminNavigation = () => {
       title: 'Tambah Admin',
       icon: AddCircle,
       path: '/register-admin',
-      openInNewTab: false
-    },
-    {
-      title: 'Dokumentasi',
-      icon: TextSnippetOutlinedIcon,
-      path: '/dokumentasi',
       openInNewTab: false
     },
     {
