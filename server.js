ed9423fb6b8d3c866d9b5ba6767ada79cb0c3dee
Gesang Paudra J <g.paudra@gmail.com>
2023-10-08 16:49:34 +0700

    update frontend server

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+// Frontend Server
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname } = parsedUrl;
+    const parsedUrl = parse(req.url, true)
+    const { pathname } = parsedUrl
 
-    handle(req, res, parsedUrl);
+    handle(req, res, parsedUrl)
   }).listen(3000, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
-});
+    console.log('> Ready on http://localhost:3000')
+    console.log(`NODE_ENV=${process.env.NODE_ENV}`)
+  })
+})
