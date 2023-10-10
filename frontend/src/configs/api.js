22a40be67e150fb5403c5469c2973823017d30d6
Gesang Paudra J <g.paudra@gmail.com>
2023-10-10 21:26:04 +0700

    Update api.js

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,7 +1,10 @@
 // src/config/api.js
-const dotenv = require('dotenv')
-dotenv.config()
+//const dotenv = require('dotenv')
+//dotenv.config()
+
+require('dotenv').config()
 
 const API_URL = process.env.API_URL
+console.log('API : ', API_URL)
 
 module.exports = API_URL
