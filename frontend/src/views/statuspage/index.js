27b7a291a3c07d7fa769b4ff6e4e24902612faf8
Gesang Paudra J <g.paudra@gmail.com>
2023-10-15 20:35:57 +0700

    update status page

--- frontend/src/views/statuspage/index.js
+++ frontend/src/views/statuspage/index.js
@@ -13,29 +13,42 @@ const DemoGrid = styled(Grid)(({ theme }) => ({
   [theme.breakpoints.down('sm')]: {
     paddingTop: `${theme.spacing(1)} !important`,
   },
-}));
+}))
 
 const StatusPage = () => {
-  const [PostgreStatus, setPostgreStatus] = useState('Loading');
-  const [serverStatus, setServerStatus] = useState('Loading');
+  const [PostgreStatus, setPostgreStatus] = useState('Loading..')
+  const [serverStatus, setServerStatus] = useState('Loading..')
+  const [CSRF, setCSRFNumber] = useState('Loading..')
+  const [AccountName, setAccountName] = useState('Loading..')
+  const [AccountEmail, setAccountEmail] = useState('Loading..')
+  const [AccountID, setAccountID] = useState('Loading..')
+  const [AccountDate, setAccountDate] = useState('Loading')
 
   useEffect(() => {
+
+    const SessionDatas = JSON.parse(sessionStorage.getItem('sessionData'))
+    setCSRFNumber(SessionDatas.csrfToken)
+    setAccountName(SessionDatas.username)
+    setAccountEmail(SessionDatas.email)
+    setAccountID(SessionDatas.id_akun)
+    setAccountDate(SessionDatas.tanggal_akun)
+
 
     // Ambil status PostgreSQL
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/postgres-status`)
       .then((response) => {
         if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+          throw new Error(`HTTP error! Status: ${response.status}`)
         }
 
-        return response.json();
+        return response.json()
       })
       .then((data) => {
-        setPostgreStatus(data.isConnected ? 'Online' : 'Offline');
+        setPostgreStatus(data.isConnected ? 'Online' : 'Offline')
       })
       .catch((error) => {
-        console.error('Error:', error);
-        setPostgreStatus('Error');
+        console.error('Error:', error)
+        setPostgreStatus('Error')
       })
 
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/server-status`)
@@ -44,14 +57,14 @@ const StatusPage = () => {
           throw new Error(`HTTP error! Status: ${response.status}`)
         }
 
-        return response.json();
+        return response.json()
       })
       .then((data) => {
         setServerStatus(data.status)
       })
       .catch((error) => {
         console.error('Error:', error)
-        setServerStatus('Error');
+        setServerStatus('Error')
       })
 
   })
@@ -94,14 +107,29 @@ const StatusPage = () => {
                 API : {process.env.NEXT_PUBLIC_API_URL}
               </Typography>
               <Typography variant="body1">
-
+                Current Session :
+              </Typography>
+              <Typography variant="body1">
+                Account ID : {AccountID}
+              </Typography>
+              <Typography variant="body1">
+                Name       : {AccountName}
+              </Typography>
+              <Typography variant="body1">
+                Email      : {AccountEmail}
+              </Typography>
+              <Typography variant="body1">
+                Account Created: {AccountDate}
+              </Typography>
+              <Typography variant="body1">
+                CSRF : {CSRF}
               </Typography>
             </DemoGrid>
           </Grid>
         </CardContent>
       </Card>
     </div>
-  );
+  )
 }
 
-export default StatusPage;
+export default StatusPage
