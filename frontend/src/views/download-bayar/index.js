98825d3ef8297d438c0e18fd75474117bc9e3da1
Gesang Paudra J <g.paudra@gmail.com>
2023-10-17 20:30:48 +0700

    trying to print lunas instead download

--- frontend/src/views/download-bayar/index.js
+++ frontend/src/views/download-bayar/index.js
@@ -250,31 +250,36 @@ const TableBayarDownload = () => {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(DownloadData),
-      })
+      });
+
       if (response.ok) {
         // Convert the response to a Blob
-        const blob = await response.blob()
+        const blob = await response.blob();
 
         // Create a URL for the Blob
-        const url = window.URL.createObjectURL(blob)
+        const url = window.URL.createObjectURL(blob);
 
-        // Create an anchor element to trigger the download
-        const a = document.createElement('a')
-        a.href = url
-        a.download = `kasbon-${nama_user}-${id_request}.docx` // Modify the file name as needed
+        // Create an iframe to display the content
+        const iframe = document.createElement('iframe');
+        iframe.src = url;
+        iframe.style.display = 'none';
 
-        // Trigger the click event to download the file
-        a.click()
+        // Append the iframe to the document
+        document.body.appendChild(iframe);
+
+        // Wait for the iframe to load, then trigger the print dialog
+        iframe.onload = () => {
+          iframe.contentWindow.print(); // Open the print dialog
+        };
 
         // Clean up resources
-        window.URL.revokeObjectURL(url)
+        window.URL.revokeObjectURL(url);
       } else {
-        console.error('Error downloading the DOCX file')
+        console.error('Error downloading the DOCX file');
         // Handle the error
       }
-    }
-    catch (error) {
-      console.error('Error :', error)
+    } catch (error) {
+      console.error('Error :', error);
     }
   }
 
