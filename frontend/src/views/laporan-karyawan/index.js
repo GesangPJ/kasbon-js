889f440e21a80a037fb4f34f7d9c1511b94fa94f
Gesang Paudra J <g.paudra@gmail.com>
2023-10-22 11:08:07 +0700

    fix mistake in laporan karyawan page

--- frontend/src/views/laporan-karyawan/index.js
+++ frontend/src/views/laporan-karyawan/index.js
@@ -163,24 +163,7 @@ const TableBayarDownload = () => {
       return
     }
 
-    // Check if the selectedDate matches the MM/YYYY format
-    const datePattern = /^(0[1-9]|1[0-2])\/\d{4}$/;
-    if (!datePattern.test(selectedDate)) {
-      setErrorMessage('Bulan dan Tahun harus dalam format MM/YYYY');
-      setTimeout(() => {
-        setErrorMessage('');
-      }, 5000);
-      return;
-    }
-
-    const formattedDate = dayjs(selectedDate, 'MM/YYYY', true); // Parse the date
-    if (!formattedDate.isValid()) {
-      setErrorMessage('Bulan dan Tahun tidak valid');
-      setTimeout(() => {
-        setErrorMessage('');
-      }, 5000);
-      return;
-    }
+    const formattedDate = selectedDate ? dayjs(selectedDate).format('MM/YYYY') : null
 
     try {
       const requestData = {
